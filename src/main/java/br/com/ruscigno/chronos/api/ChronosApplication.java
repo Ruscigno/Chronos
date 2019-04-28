6971709299594f2485edc0406c1388805f1bddd3
Sander Ruscigno <ruscigno@gmail.com>
2019-04-28 16:11:47 -0300

    feat: enabling ehCache

--- src/main/java/br/com/ruscigno/chronos/api/ChronosApplication.java
+++ src/main/java/br/com/ruscigno/chronos/api/ChronosApplication.java
@@ -2,8 +2,10 @@ package br.com.ruscigno.chronos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ChronosApplication {
 
 	public static void main(String[] args) {
