5f0ddbb304dbe91bed55514a06a800155bd77011
Sander Ruscigno <ruscigno@gmail.com>
2019-05-01 02:06:26 -0300

    fix: downgrade to swagger 2.6

--- src/main/java/br/com/ruscigno/chronos/api/config/SwaggerConfig.java
+++ src/main/java/br/com/ruscigno/chronos/api/config/SwaggerConfig.java
@@ -13,6 +13,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.ApiKeyVehicle;
@@ -38,16 +39,19 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Employee Time Tracking API")
-				.description("Documentation of Employee Time Tracking.").version("1.0")
-				.build();
+		return new ApiInfoBuilder()
+			.title("Chronos Employee Time Tracking API")
+			.description("RESTfull API to show the power of Java and Spring Boot.")
+			.contact(new Contact("Sander Ruscigno", "https://www.ruscigno.com.br", "ruscigno@gmail.com"))
+			.version("1.0")
+			.build();
 	}
 
 	@Bean
 	public SecurityConfiguration security() {
 		String token;
 		try {
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@kazale.com");
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@ruscigno.com");
 			token = this.jwtTokenUtil.obterToken(userDetails);
 		} catch (Exception e) {
 			token = "";
---
+++ src/main/resources/db/migration/postgres/V3__lancamento_padrao.sql
@@ -0,0 +1,2 @@
+insert into lancamento(id, data,data_atualizacao, data_criacao,descricao,localizacao,tipo,funcionario_id)
+values(0, now(), now(), now(),'entrada','Sede','INICIO_TRABALHO',1);
